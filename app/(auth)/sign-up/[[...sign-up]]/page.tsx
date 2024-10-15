b45ece11b3f36e20a04fe130738b7b4e875b8146
YogeshDhengale <yd30668497@gmail.com>
2024-10-15 15:11:18 +0530

    removig the getStatic params

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,10 @@
 import { SignUp } from "@clerk/nextjs";
 import React from "react";
 
+export async function generateStaticParams() {
+  return [{ signUp: [] }];
+}
+
 function SignUpPage() {
   return (
     <main className="w-full flex-center h-screen">
